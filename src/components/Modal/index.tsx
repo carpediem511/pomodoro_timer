0db284786b76e114fa5bb1e5f3e326ca1584bfa9
carpediem511 <vampire.0514@gmail.com>
2023-10-07 19:20:36 +0300

    готов таймер без настроек и модалки

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,14 +1,11 @@
-import React from 'react';
+import React from "react";
 //import { Dialog, Transition } from '@headlessui/react';
 //import { Fragment } from 'react';
 
-
-const Modal = ({ }) => {
-
-
-	return (
-		<>
-			{/*		<Transition appear show={isOpen} as={Fragment}>
+const Modal = ({}) => {
+  return (
+    <>
+      {/*		<Transition appear show={isOpen} as={Fragment}>
 				<Dialog as="div" className="relative z-10" onClose={onClose}>
 					<Transition.Child
 						as={Fragment}
@@ -59,8 +56,8 @@ const Modal = ({ }) => {
 					</div>
 				</Dialog>
 			</Transition>*/}
-		</>
-	);
+    </>
+  );
 };
 
 export default Modal;
--- src/components/Settings/index.tsx
+++ src/components/Settings/index.tsx
@@ -1,47 +1,50 @@
-import Slider from '@mui/material-next/Slider'
+import Slider from "@mui/material-next/Slider";
 //import Modal from './Modal'; // Импортируйте компонент Modal
 //import Timer from './Timer'; // Импортируйте компонент Timer
 //import { useState } from 'react';
 
 const Settings = () => {
+  const workSliderStyle = {
+    color: "red",
+  };
 
-	const workSliderStyle = {
-		color: 'red',
-	}
+  const restSliderstyle = {
+    color: "green",
+  };
 
-	const restSliderstyle = {
+  return (
+    <>
+      <p className="text-teal-600 font-bold text-lg">Настройки:</p>
+      <div className="flex w-full flex-col">
+        <label className="mt-10 text-teal-600 text-base font-semibold">
+          Время для работы:
+        </label>
+        <Slider
+          max={120}
+          min={25}
+          step={5}
+          valueLabelDisplay="on"
+          className="mt-10"
+          style={workSliderStyle}
+        />
+        <label className="mt-5 text-teal-600 text-base font-semibold">
+          Время для отдыха:
+        </label>
+        <Slider
+          max={60}
+          min={0}
+          step={5}
+          valueLabelDisplay="on"
+          className="mt-10"
+          style={restSliderstyle}
+        />
 
-		color: 'green'
-	}
+        <button className="border p-1 w-1/2 mx-auto mt-8 rounded-md text-white font-semibold active:shadow-lg flex justify-center bg-teal-600 hover:bg-teal-400 duration-200 hover:text-black">
+          Подтвердить
+        </button>
+      </div>
+    </>
+  );
+};
 
-
-	return (
-		<>
-			<p className='text-teal-600 font-bold text-lg'>Настройки:</p>
-			<div className="flex w-full flex-col">
-				<label className="mt-10 text-teal-600 text-base font-semibold">Время для работы:</label>
-				<Slider
-					max={120}
-					min={25}
-					step={5}
-					valueLabelDisplay="on"
-					className='mt-10'
-					style={workSliderStyle}
-				/>
-				<label className='mt-5 text-teal-600 text-base font-semibold'>Время для отдыха:</label>
-				<Slider
-					max={60}
-					min={0}
-					step={5}
-					valueLabelDisplay="on"
-					className='mt-10'
-					style={restSliderstyle}
-				/>
-
-				<button className='border p-1 w-1/2 mx-auto mt-8 rounded-md text-white font-semibold active:shadow-lg flex justify-center bg-teal-600 hover:bg-teal-400 duration-200 hover:text-black'>Подтвердить</button>
-			</div>
-		</>
-	)
-}
-
-export default Settings
+export default Settings;
