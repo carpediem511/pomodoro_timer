f62ea3176f62d1ec8983082a1ba239ed63ed62bb
carpediem511 <vampire.0514@gmail.com>
2023-10-07 16:59:26 +0300

    таймер работает, но неуправляем/таймер

--- src/components/Timer/index.tsx
+++ src/components/Timer/index.tsx
@@ -10,18 +10,52 @@ import Modal from '../Modal';
 
 
 const Timer = ({ }) => {
-	const [minutes, setMinutes] = useState<number>(/*workTime*/)
-	const [seconds, setSeconds] = useState<number>(0);
+	const [minutes, setMinutes] = useState<number>(25)
+	const [seconds, setSeconds] = useState<number>(0)
+	const [timerType, setTimerType] = useState<boolean>(false)
+	const [isModalOpen, setIsModalOpen] = useState<boolean>(false)
 
+	const time: string = `${minutes < 10 ? '0' + minutes : minutes} : ${seconds < 10 ? '0' : ''}${seconds}`
+
+	const playSound = (soundFile: string) => {
+		const audio = new Audio(soundFile);
+		audio.play();
+	}
+
+	let timer: any
+	useEffect(() => {
+
+		timer = setInterval(() => {
+
+			setSeconds(seconds + 1)
+
+			if (seconds === 0) {
+				if (minutes !== 0) {
+
+					setSeconds(59)
+					setMinutes(minutes - 1)
+				} else {
+
+
+				}
+			} else {
+
+				setSeconds(seconds - 1)
+			}
+		}, 1000)
+
+		return () => clearInterval(timer)
+	})
 
 	return (
 		<>
 			<CircularProgressbar
-				value={25}
+				value={(minutes * 60 + seconds)}
+				text={time}
 				styles={buildStyles({
 					textColor: 'rgb(109, 40, 217)',
-					trailColor: 'rgb(91, 33, 182)',
-					pathColor: 'rgb(40, 114, 216)'
+					trailColor: 'rgb(200, 200, 200)',
+					pathColor: 'rgb(138, 43, 226)'
 				})}
 			/>
 			<div className="flex justify-center mt-10 gap-4">
