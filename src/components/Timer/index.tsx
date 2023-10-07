e5c9e01244cd126b01737fa25f299cfbd6996209
carpediem511 <vampire.0514@gmail.com>
2023-10-07 15:46:39 +0300

    добавила кнопку для запуска/таймер

--- src/components/Timer/index.tsx
+++ src/components/Timer/index.tsx
@@ -21,10 +21,15 @@ const Timer = ({ }) => {
 				styles={buildStyles({
 					textColor: 'rgb(109, 40, 217)',
 					trailColor: 'rgb(91, 33, 182)',
-					pathColor: 'rgb(40, 114, 216)', // Новый цвет для заполнения
+					pathColor: 'rgb(40, 114, 216)'
 				})}
 			/>
 			<div className="flex justify-center mt-10 gap-4">
+
+				<button className='self-center text-white font-semibold active:shadow-lg flex rounded-lg px-3 mt-2 py-2 border w-1/2 justify-center bg-teal-600 hover:bg-teal-400 duration-200 hover:text-black'>
+					<PlayCircleIcon className='w-1/4 text-white' />
+					Начинаем!
+				</button>
 
 				<button className='self-center text-white font-semibold active:shadow-lg flex rounded-lg px-3 mt-2 py-2 border w-1/2 justify-center bg-teal-600 hover:bg-teal-400 duration-200 hover:text-black'>
 					<Cog6ToothIcon className='w-1/4 text-white' />
