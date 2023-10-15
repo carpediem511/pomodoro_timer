2182acf3a9dabaf96b9bc142011211e870066802
carpediem511 <vampire.0514@gmail.com>
2023-10-15 17:23:35 +0300

    //

--- src/components/Timer/index.tsx
+++ src/components/Timer/index.tsx
@@ -4,30 +4,31 @@ import "react-circular-progressbar/dist/styles.css";
 import TimerButtons from "../TimerButtons";
 import ModalNotification from "../ModalNotification";
 
+
 const Timer = () => {
-	const [totalMinutes, setTotalMinutes] = useState<number>(25); // общая продолжительность в минутах
-	const [timerType, setTimerType] = useState<string>("work"); // текущий тип таймера: работа/отдых
-	const [isTimerOn, setIsTimerOn] = useState<boolean>(false); // запущен ли таймер: да/нет
-	const [minutesToEnd, setMinutesToEnd] = useState<number>(totalMinutes); // оставшиеся минуты
-	const [secondsToEnd, setSecondsToEnd] = useState<number>(0); // оставшиеся секунды
-	const [isOpenModal, setIsOpenModal] = useState<boolean>(false); // открыто ли модальное окно
-	const [progress, setProgress] = useState<string>(""); // прогресс в кружке
+	const [totalMinutes, setTotalMinutes] = useState<number>(25);
+	const [timerType, setTimerType] = useState<string>("work");
+	const [isTimerOn, setIsTimerOn] = useState<boolean>(false);
+	const [minutesToEnd, setMinutesToEnd] = useState<number>(totalMinutes);
+	const [secondsToEnd, setSecondsToEnd] = useState<number>(0);
+	const [isOpenModal, setIsOpenModal] = useState<boolean>(false);
+	const [progress, setProgress] = useState<string>("");
 	const [notificationMessage, setNotificationMessage] = useState<string>("");
 
-	// время работы и отдыха
 	const workTime = 25;
 	const restTime = 5;
-	let timerInterval: any;
+	let timerInterval: any
 
 	const playSound = (soundFile: string) => {
 		const audio = new Audio(soundFile);
 		audio.play();
 	};
 
-	// переключение таймера
 	const toggleTimer = () => {
 		if (isTimerOn) {
-			clearInterval(timerInterval);
+			if (timerInterval) {
+				clearInterval(timerInterval);
+			}
 			setIsTimerOn(false);
 		} else {
 			if (minutesToEnd > 0 || secondsToEnd > 0) {
@@ -37,9 +38,10 @@ const Timer = () => {
 		}
 	};
 
-	// сброс таймера
 	const resetTimer = () => {
-		clearInterval(timerInterval);
+		if (timerInterval) {
+			clearInterval(timerInterval);
+		}
 		setIsTimerOn(false);
 		setMinutesToEnd(totalMinutes);
 		setSecondsToEnd(0);
@@ -47,7 +49,6 @@ const Timer = () => {
 		setProgress("");
 	};
 
-	// запуск интервала для обратного отсчёта
 	const startInterval = () => {
 		timerInterval = setInterval(() => {
 			if (secondsToEnd === 0) {
@@ -60,25 +61,23 @@ const Timer = () => {
 			} else {
 				setSecondsToEnd(secondsToEnd - 1);
 			}
-
-			const progressValue: string = `${minutesToEnd}:${secondsToEnd < 10 ? `0${secondsToEnd}` : secondsToEnd
-				}`;
+			// eslint-disable-next-line
+			const progressValue: string = `${minutesToEnd}:${secondsToEnd < 10 ? `0${secondsToEnd}` : secondsToEnd}`;
 			setProgress(progressValue);
 		}, 1000);
 	};
 
-	// обработчик завершения таймера
 	const handleTimerEnd = () => {
-		clearInterval(timerInterval);
+		if (timerInterval) {
+			clearInterval(timerInterval);
+		}
 		setIsTimerOn(false);
 		if (timerType === "work") {
 			playSound("/rest_time.mp3");
 			setIsOpenModal(true);
 			setTimerType("rest");
 			setTotalMinutes(restTime);
-			setNotificationMessage(
-				"Вы хорошенько поработали! Так держать! Теперь немного отдохните!",
-			);
+			setNotificationMessage("Вы хорошенько поработали! Так держать! Теперь немного отдохните!");
 		} else {
 			playSound("/work_time.mp3");
 			setIsOpenModal(true);
@@ -91,33 +90,27 @@ const Timer = () => {
 	useEffect(() => {
 		if (isTimerOn) {
 			startInterval();
-		} else {
+		} else if (timerInterval) {
 			clearInterval(timerInterval);
 		}
 
 		return () => {
-			clearInterval(timerInterval);
-		};// eslint-disable-next-line
-	}, [isTimerOn, secondsToEnd, minutesToEnd, timerInterval]);
+			if (timerInterval) {
+				clearInterval(timerInterval);
+			}
+		};
+	}, [isTimerOn, secondsToEnd, minutesToEnd, startInterval, timerInterval]);
 
-	const time: string = `${minutesToEnd < 10 ? "0" + minutesToEnd : minutesToEnd
-		}:${secondsToEnd < 10 ? "0" : ""}${secondsToEnd}`;
+	const time: string = `${minutesToEnd < 10 ? "0" + minutesToEnd : minutesToEnd}:${secondsToEnd < 10 ? "0" : ""}${secondsToEnd}`;
 
-	const timerTitle =
-		timerType === "work" ? "Время для работы" : "Время для отдыха";
+	const timerTitle = timerType === "work" ? "Время для работы" : "Время для отдыха";
+
 	return (
 		<>
 			<div className="text-center mt-8">
-				<h2
-					className={`text-2xl mb-6 font-bold ${timerType === "work" ? "text-red-700" : "text-green-600"
-						}`}
-				>
-					{timerTitle}
-				</h2>
+				<h2 className={`text-2xl mb-6 font-bold ${timerType === "work" ? "text-red-700" : "text-green-600"}`}>{timerTitle}</h2>
 				<CircularProgressbar
-					value={
-						((minutesToEnd * 60 + secondsToEnd) / (totalMinutes * 60)) * 100
-					}
+					value={((minutesToEnd * 60 + secondsToEnd) / (totalMinutes * 60)) * 100}
 					text={time}
 					styles={buildStyles({
 						textColor: "rgb(109, 40, 217)",
@@ -133,11 +126,8 @@ const Timer = () => {
 
 				{isOpenModal && (
 					<ModalNotification
-						message={
-							timerType === "work"
-								? "Снова пора за работу!"
-								: "Вы хорошенько поработали! Так держать! Теперь немного отдохните!"
-						}
+						// eslint-disable-next-line
+						message={timerType === "work" ? "Снова пора за работу!" : "Вы хорошенько поработали! Так держать! Теперь немного отдохните!"}
 						onClose={() => setIsOpenModal(false)}
 						resetTimer={resetTimer}
 					/>
